dceef627754659e01accd395f28b6f1e63bac7ba
Sarapuce <mrolo76olo@gmail.com>
2022-05-26 23:00:40 +0200

    Added some comments

--- SnipOff7.py
+++ SnipOff7.py
@@ -7,21 +7,24 @@ def add_file(path):
     name = path.split('\\')[-1]
     files[name] = content
 
-files = {}
-offsets = {}
+if __name__ == "__main__":
+    files = {}
+    offsets = {}
 
-config = configparser.ConfigParser()
-config.read("config.ini")
+    config = configparser.ConfigParser()
+    config.read("config.ini")
 
-for section in config.sections():
-    if "path" in config[section].keys():
-        add_file(config[section]["path"])
-        offsets[section] = config[section]["offset"]
+    # Find all file descriptions in the config file
+    for section in config.sections():
+        if "path" in config[section].keys():
+            add_file(config[section]["path"])
+            offsets[section] = config[section]["offset"]
 
-for section in config.sections():
-    if "file" in config[section].keys():
-        pattern = bytearray.fromhex(config[section]["pattern"])
-        file = config[section]["file"]
-        function_offset = files[file].find(pattern) + int(offsets[file][2:], 16)
-        print("[+] {}+{} : {}".format(file, hex(function_offset)[2:], section))
+    # Find all functions
+    for section in config.sections():
+        if "file" in config[section].keys():
+            pattern = bytearray.fromhex(config[section]["pattern"])
+            file = config[section]["file"]
+            function_offset = files[file].find(pattern) + int(offsets[file][2:], 16) # Add to add an offset for each file
+            print("[+] {}+{} : {}".format(file, hex(function_offset)[2:], section))
 
